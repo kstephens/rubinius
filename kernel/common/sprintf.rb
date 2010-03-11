c59a3e56590b1863b523f615f6ec860e01c95923
Kurt Stephens <ks.github@kurtstephens.com>
2010-03-11 00:05:28 -0600

    Improve performance of sprintf.rb by ~ 29%. Avoid String garbage by using CONSTANTS. Use instance variables instead of getters. Use instance variables instead of temporary Hashes. Avoid [ ... ].include? Cache Slot instances.

--- kernel/common/sprintf.rb
+++ kernel/common/sprintf.rb
@@ -3,11 +3,50 @@ module Rubinius
 
     attr_accessor :fmt
     attr_accessor :args
-    attr_accessor :flags
+
+    attr_accessor :flags_position
+    attr_accessor :flags_space
+    attr_accessor :flags_zero
+    attr_accessor :flags_star
+    attr_accessor :flags_plus
+    attr_accessor :flags_minus
+    attr_accessor :flags_alternative
 
     RADIXES = {"b" => 2, "o" => 8, "d" => 10, "x" => 16}
+    RADIXES.each { | k, v | RADIXES[k.upcase] = v }
+    RADIXES.freeze
     ALTERNATIVES = {"o" => "0", "b" => "0b", "B" => "0B", "x" => "0x", "X" => "0X"}
     PrecisionMax = 1048576 # Totally random value
+
+    PERCENT = "%".freeze
+    SPACE = " ".freeze
+    ZERO = "0".freeze
+    EMPTY_STRING = "".freeze
+    PLUS = "+".freeze
+    MINUS = "-".freeze
+    STAR = "*".freeze
+    HASH = "#".freeze
+    DOTDOT = '..'.freeze
+    E = 'E'.freeze
+    E_LC = 'e'.freeze
+    F = 'F'.freeze
+    F_LC = 'f'.freeze
+    G = 'G'.freeze
+    G_LC = 'g'.freeze
+    U_LC = "u".freeze
+    D_LC = "d".freeze
+    I_LC = "i".freeze
+    S_LC = "s".freeze
+    B    = "B".freeze
+    B_LC = "b".freeze
+    C_LC = "c".freeze
+    O_LC = "o".freeze
+    P_LC = "p".freeze
+    X    = "X".freeze
+    X_LC = "x".freeze
+    NAN = "NaN".freeze
+    INF = "Inf".freeze
+    N_INF = "-Inf".freeze
 
     def initialize(fmt, *args)
       @tainted = fmt.tainted?
@@ -24,25 +63,31 @@ module Rubinius
       @arg_position = 0
 
       while (match = /%/.match_from(fmt, start))
+        @flags_position =
+          @flags_space =
+          @flags_zero =
+          @flags_star =
+          @flags_plus =
+          @flags_minus =
+          @flags_alternative =
+          nil
 
-        @flags = {:space => nil, :position => nil, :alternative => nil, :plus => nil,
-          :minus => nil, :zero => nil, :star => nil}
         @width = @precision = @type = nil
 
         ret << match.pre_match_from(start)
         start = match.begin(0) + 1
 
         # Special case: %% prints out as "%"
-        if [?\n, 0].include?(@fmt[start])
-          ret << "%" << @fmt[start]
+        if (fmt_start = @fmt[start]) == ?\n || fmt_start == 0
+          ret << PERCENT << @fmt[start]
           start += 1
           next
-        elsif [?%, nil].include?(@fmt[start])
-          ret << "%"
+        elsif fmt_start == ?% || fmt_start.nil?
+          ret << PERCENT
           start += 1
           next
         elsif @fmt[start, 3] =~ /[1-9]\$/ && !@fmt[start + 2]
-          ret << "%"
+          ret << PERCENT
           start = @fmt.size
           break
         end
@@ -52,32 +97,32 @@ module Rubinius
           case token[0]
           # Special case: if we get two [1-9]\$, it means that we're outside of flag-land
           when /[1-9]\$/
-            raise ArgumentError, "value given twice - #{token[0]}" if flags[:position]
-            @flags[:position] = token[0][0].chr.to_i
+            raise ArgumentError, "value given twice - #{token[0]}" if @flags_position
+            @flags_position = token[0][0].chr.to_i
             start += 1
-          when " "
-            @flags[:space] = true
-          when "#"
-            @flags[:alternative] = true
-          when "+"
-            @flags[:plus] = true
-          when "-"
-            @flags[:minus] = true
-          when "0"
-            @flags[:zero] = true
-          when "*"
-            raise ArgumentError, "width given twice" if flags[:star]
+          when SPACE
+            @flags_space = true
+          when HASH
+            @flags_alternative = true
+          when PLUS
+            @flags_plus = true
+          when MINUS
+            @flags_minus = true
+          when ZERO
+            @flags_zero = true
+          when STAR
+            raise ArgumentError, "width given twice" if @flags_star
             if width_dollar_match = /\G[1-9]\$/.match_from(fmt, start + 1)
-              @width = Slot.new('*' << width_dollar_match[0])
+              @width = Slot.new("*" << width_dollar_match[0])
               start += 2
             end
-            @flags[:star] = true
+            @flags_star = true
           end
           start += 1
         end
 
         # WIDTH STATE
-        if !flags[:star] && width_match = /\G([1-9]\$|\*|\d+)/.match_from(fmt, start)
+        if !@flags_star && width_match = /\G([1-9]\$|\*|\d+)/.match_from(fmt, start)
           @width = Slot.new(width_match[0])
           start += width_match[0].size
         end
@@ -88,23 +133,23 @@ module Rubinius
           # PRECISION STATE
           if /\G\*/.match_from(fmt, start)
             if precision_dollar_match = /\G[1-9]\$/.match_from(fmt, start + 1)
-              @precision = Slot.new('*' << precision_dollar_match[0])
+              @precision = Slot.new("*" << precision_dollar_match[0])
               start += 3
             else
-              @precision = Slot.new('*')
+              @precision = Slot.new(STAR)
               start += 1
             end
           elsif precision_match = /\G([1-9]\$|\d+)/.match_from(fmt, start)
             @precision = Slot.new(precision_match[0])
             start += precision_match[0].size
           else
-            @precision = Slot.new("0")
+            @precision = Slot.new(ZERO)
           end
 
           # check for positional value again, after the optional '.'
           if positional_match = /\G[1-9]\$/.match_from(fmt, start)
-            raise ArgumentError, "value given twice - #{token[0]}" if flags[:position]
-            @flags[:position] = positional_match[0][0].chr.to_i
+            raise ArgumentError, "value given twice - #{token[0]}" if @flags_position
+            @flags_position = positional_match[0][0].chr.to_i
             start += 2
           end
         end
@@ -122,7 +167,7 @@ module Rubinius
         ret << f if f
       end
       if $DEBUG == true && !@positional
-        raise ArgumentError, "you need to use all the arguments" unless @arg_position == args.size
+        raise ArgumentError, "you need to use all the arguments" unless @arg_position == @args.size
       end
       ret << @fmt[start..-1] if start < @fmt.size
       ret.taint if @tainted
@@ -131,34 +176,34 @@ module Rubinius
 
     def format
       # GET VALUE
-      if flags[:position]
-        val = Slot.new("#{flags[:position]}$")
+      if @flags_position
+        val = Slot.new("#{@flags_position}$")
         val = get_arg(val)
       end
 
       # GET WIDTH
-      @width = Slot.new("*") if flags[:star] && !@width
+      @width = Slot.new(STAR) if @flags_star && !@width
       width = get_arg(@width)
       width = width.to_int if width.respond_to?(:to_int)
       if width && width < 0
         width = width.abs
-        flags[:minus] = true
+        @flags_minus = true
       end
 
       # GET PRECISION
       precision = get_arg(@precision)
       precision = precision.to_int if precision.respond_to?(:to_int)
 
-      unless flags[:position]
-        val = Slot.new("*")
+      unless @flags_position
+        val = Slot.new(STAR)
         val = get_arg(val)
       end
 
       case @type
-      when "e", "E", "f", "g", "G"
-        if @type.downcase == "g" && flags[:alternative]
-          @old_type = "g"
-          @type = "f"
+      when E_LC, E, F_LC, G_LC, G
+        if (@type == G_LC || @type == G) && @flags_alternative
+          @old_type = G_LC
+          @type = F_LC
           precision = 4 unless precision
         end
         val = Float(val)
@@ -166,24 +211,24 @@ module Rubinius
           ret = val.send(:to_s_formatted, build_format_string(width, precision))
           ret = plus_char + ret if val >= 0 && @old_type
         else
-          ret = (val < 0 ? "-Inf" : "Inf") if val.infinite?
-          ret = "NaN" if val.nan?
+          ret = (val < 0 ? N_INF : INF) if val.infinite?
+          ret = NAN if val.nan?
           ret = plus_char + ret if val > 0
-          flags[:zero] = flags[:space] = flags[:plus] = nil
+          @flags_zero = @flags_space = @flags_plus = nil
           ret = pad(ret, width, precision)
         end
-      when "u"
+      when U_LC
         val = get_number(val)
         if val < 0
           unless val.kind_of?(Fixnum)
             raise ArgumentError, "invalid type (only Fixnum allowed)"
           end
 
-          plus_or_space = flags[:space] || flags[:plus]
+          plus_or_space = @flags_space || @flags_plus
           unless plus_or_space
             val = (1 << (2.size * 8)) + val
           end
-          unless flags[:zero] or precision or plus_or_space
+          unless @flags_zero or precision or plus_or_space
             ret = "..#{pad(val, width, precision)}"
           else
             ret = pad(val, width, precision)
@@ -191,37 +236,37 @@ module Rubinius
         else
           ret = pad(val, width, precision)
         end
-      when "d", "i"
+      when D_LC, I_LC
         val = get_number(val)
         ret = pad(val, width, precision)
-      when "c"
+      when C_LC
         val = val.to_int if val.respond_to?(:to_int)
         raise TypeError, "cannot convert #{val.class} into Integer" unless val.respond_to?(:chr) && val.respond_to?(:%)
         val = (val % 256).chr
         ret = pad(val, width, precision)
-      when "s"
-        flags[:zero] = flags[:space] = flags[:plus] = nil
+      when S_LC
+        @flags_zero = @flags_space = @flags_plus = nil
         ret = pad(val, width, precision)
         ret.taint if val.tainted?
-      when "p"
-        flags[:zero] = flags[:space] = flags[:plus] = nil
+      when P_LC
+        @flags_zero = @flags_space = @flags_plus = nil
         ret = pad(val.inspect, width, precision)
-      when "o", "x", "X", "b", "B"
+      when O_LC, X_LC, X, B_LC, B
         val = get_number(val)
-        unless flags[:space] || flags[:plus]
-          ret = Number.new(val, RADIXES[@type.downcase]).rep
-          chr = val < 0 ? (RADIXES[@type.downcase] - 1).to_s(RADIXES[@type.downcase]) : 0.to_s
+        unless @flags_space || @flags_plus
+          ret = Number.new(val, RADIXES[@type]).rep
+          chr = val < 0 ? (RADIXES[@type] - 1).to_s(RADIXES[@type]) : ZERO
           ret = pad(ret, width, precision, chr)
-          ret = ALTERNATIVES[@type].to_s + ret if flags[:alternative]
+          ret = (ALTERNATIVES[@type] ||= ''.freeze) + ret if @flags_alternative
         else
-          flags[:plus] = nil if val < 0
-          ret = val.to_s(RADIXES[@type.downcase])
-          ret.gsub!(/^(\-?)/, "\1#{ALTERNATIVES[@type]}") if flags[:alternative]
+          @flags_plus = nil if val < 0
+          ret = val.to_s(RADIXES[@type])
+          ret.gsub!(/^(\-?)/, "\1#{ALTERNATIVES[@type]}") if @flags_alternative
           ret = pad(ret, width, precision)
-          ret.gsub!(/ \-/, "-")
+          ret.gsub!(/ \-/, MINUS)
         end
-        ret = ret.downcase if @type == "x"
-        ret = ret.upcase if @type == "X"
+        ret = ret.downcase if @type == X_LC
+        ret = ret.upcase if @type == X
       end
       ret
     end
@@ -248,11 +293,11 @@ module Rubinius
 
     def make_flags
       ret = ""
-      ret << " " if flags[:space]
-      ret << "#" if flags[:alternative]
-      ret << "+" if flags[:plus]
-      ret << "-" if flags[:minus]
-      ret << "0" if flags[:zero]
+      ret << SPACE if @flags_space
+      ret << HASH  if @flags_alternative
+      ret << PLUS  if @flags_plus
+      ret << MINUS if @flags_minus
+      ret << ZERO  if @flags_zero
       ret
     end
 
@@ -263,13 +308,13 @@ module Rubinius
       when slot.position == :next
         raise ArgumentError, "unnumbered mixed with numbered" if @positional
         @relative = true
-        raise ArgumentError, "you ran out of arguments" if @arg_position >= args.size
-        ret = args[@arg_position]
+        raise ArgumentError, "you ran out of arguments" if @arg_position >= @args.size
+        ret = @args[@arg_position]
         @arg_position += 1
       when slot.pos
         raise ArgumentError, "unnumbered mixed with numbered" if @relative
         @positional = true
-        ret = args[slot.position - 1]
+        ret = @args[slot.position - 1]
       when slot.value
         raise ArgumentError, "unnumbered mixed with numbered" if @positional
         @relative = true
@@ -282,30 +327,30 @@ module Rubinius
     end
 
     def pad(val, width, precision, pad_override = nil)
-      direction = flags[:minus] ? :ljust : :rjust
+      direction = @flags_minus ? :ljust : :rjust
       ret = val.to_s
-      modded_width = width.to_i + (flags[:plus] ? 1 : 0)
+      modded_width = width.to_i + (@flags_plus ? 1 : 0)
       width = nil if modded_width <= val.to_s.size
       if ret[0] != ?-
         sign = plus_char
       else
-        sign = ""
+        sign = EMPTY_STRING
       end
 
-      if precision || flags[:zero]
-        ret.gsub!("..", "")
+      if precision || @flags_zero
+        ret.gsub!(DOTDOT, EMPTY_STRING)
       end
       if precision
         if precision > PrecisionMax
           raise ArgumentError, "precision too big"
         end
-        ret = sign + ret.send(direction, precision, pad_override || "0")
-        flags[:zero] = flags[:plus] = flags[:space] = nil
+        ret = sign + ret.send(direction, precision, pad_override || ZERO)
+        @flags_zero = @flags_plus = @flags_space = nil
       end
       if width
-        if pad_char != " " && ret[0] == ?-
+        if pad_char != SPACE && ret[0] == ?-
           ret.slice!(0)
-          sign = "-"
+          sign = MINUS
           width -= 1
           ret = ret.rjust(width, pad_char)
         else
@@ -318,13 +363,13 @@ module Rubinius
     end
 
     def pad_char
-      flags[:zero] ? "0" : " "
+      @flags_zero ? ZERO : SPACE
     end
 
     def plus_char
-      return "+" if flags[:plus]
-      return " " if flags[:space]
-      ""
+      return PLUS if @flags_plus
+      return SPACE if @flags_space
+      EMPTY_STRING
     end
 
     class Slot
@@ -335,6 +380,12 @@ module Rubinius
       attr_reader :value
       attr_reader :str
 
+      INSTANCE_CACHE = { }
+      def self.new str
+        INSTANCE_CACHE[str] ||=
+          super(str).freeze
+      end
+
       def initialize(str)
         @pos = false
         @str = str
@@ -344,7 +395,7 @@ module Rubinius
         elsif str.size == 2 && str[1] == ?$
           @pos = true
           @position = str[0..0].to_i
-        elsif str == "*"
+        elsif str == STAR
           @position = :next
         else
           @value = str.to_i
@@ -364,7 +415,7 @@ module Rubinius
         return @number.to_s(@radix) if(@number >= 0) || @radix == 10
         strlen = (@number.to_s(@radix)).size
         max = (@pad * strlen).to_i(@radix)
-        ".." + (max + @number + 1).to_s(@radix)
+        DOTDOT + (max + @number + 1).to_s(@radix)
       end
 
     end
